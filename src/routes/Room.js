5b1e0b9d915785c47fc2116e2d5db688167dda98
김하영 <khy4018@gmail.com>
2022-12-16 19:50:56 +0900

    Modify 오름차순 내림차순(sort)

--- src/routes/Room.js
+++ src/routes/Room.js
@@ -1,7 +1,46 @@
+import {useEffect, useRef, useState} from "react"
+
 const Room = () => {
+
+  const [count, setCount] = useState(0)
+  const [list, setList] = useState([])
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  useEffect(() => {
+    console.log('mount')
+    return console.log('unmount')
+  }, [])
+
+  useEffect(() => {
+    if (count !== 0) {
+      setList([...list, {'id': list.length, 'value': count}])
+    }
+    console.log(list)
+  }, [count])
+
   return (
     <div>
       Coming soon!
+      <span>{count}</span>
+      <button type="button" onClick={increment}>+</button>
+      <button type="button" onClick={decrement}>-</button>
+
+      <ul>
+        {list.map((item, i) => (
+          <li key={`${item}-${i}`}>{item.value}</li>))}
+      </ul>
+      <ol>
+        {list.slice(0).sort((a, b) => b.value - a.value).map((item, i) => (
+          <li key={`${i}-${item}`}>{item.value}</li>
+        )) }
+      </ol>
     </div>
   )
 }
