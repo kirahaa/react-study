4907e7ae4582c9418b329ad05842ff3e57ffafd5
김하영 <khy4018@gmail.com>
2023-01-17 15:16:25 +0900

    Modify 계산기

--- src/routes/Calc/Calc.js
+++ src/routes/Calc/Calc.js
@@ -1,12 +1,28 @@
 import styled from 'styled-components'
 import classNames from "classnames/bind"
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import style from './Calc.module.scss'
 const cx = classNames.bind(style)
 
+const CalcWrap = styled.div`
+  display: flex;
+  gap: 1px;
+  background-color: ${(props) => props.theme.colors.bgLight};
+  
+  @media (max-width: 720px) {
+    flex-direction: column;
+  }
+`
+
 const CalcBox = styled.div`
-  width: 50%;
+  flex: 1;
   padding: 2rem;
+  background-color: ${(props) => props.theme.colors.bg};
+  
+  h2{
+    font-family: 'Bungee', cursive;
+    font-weight: 800;
+  }
 
   p {
     padding: 1rem 2rem;
@@ -16,11 +32,9 @@ const CalcBox = styled.div`
 
 const Input = styled.input`
   width: 100%;
-  margin-bottom: 2%;
   padding: 1rem 0;
   border-bottom: ${(props) => `1px solid ${props.theme.colors.bgLight}`};
   font-size: 1.5rem;
-  text-indent: 1rem;
   background: ${(props) => props.theme.colors.bg};
 `
 
@@ -30,48 +44,117 @@ const Button = styled.button`
 `
 
 const Calc = () => {
-  const [percentageStandard, setPercentageStandard] = useState(null)
-  const [percentageElement, setPercentageElement] = useState(null)
-  const [percentageResult, setPercentageResult] = useState(null)
+  const [percentage, setPercentage] = useState({standard: '', element: ''})
+  const [percentageResult, setPercentageResult] = useState('')
 
-  const percentageSubmit = (event) => {
-    event.preventDefault();
-    setPercentageResult((percentageElement / percentageStandard) * 100)
+  const [rem, setRem] = useState({standard: '', default: '', relative: ''})
+  const [remResult, setRemResult] = useState('')
+
+  const [proportion, setProportion] = useState({A: '', B: '', C: '', X: 'x'})
+  const [proportionResult, setProportionResult] = useState('')
+
+  const submit = (event) => {
+    event.preventDefault()
+    const {target: {id}} = event
+
+    if (id === 'percentage') {
+      setPercentageResult((percentage.element / percentage.standard) * 100)
+    } else if (id === 'rem') {
+      setRemResult((rem.default * rem.relative) / rem.standard)
+    } else {
+      setProportionResult((proportion.B * proportion.C) / proportion.A)
+    }
   }
 
   return (
-    <div>
+    <CalcWrap>
       <CalcBox>
         <h2>1. Percentage</h2>
         <p>e.g., position, width, height...</p>
-        <form className={cx('calc__forms')} onSubmit={percentageSubmit}>
-          <div className={cx('calc__form')}>
-            <label htmlFor="percentage-standard">기준 값</label>
-            <Input
-              type="text"
-              id="percentage-standard"
-              value={percentageStandard}
-              className="calc__input"
-              onChange={(event) => setPercentageStandard(event.target.value)}/>
-          </div>
-          <div className={cx('calc__form')}>
-            <label htmlFor="percentage-element">요소 값</label>
-            <Input
-              type="text"
-              id="percentage-standard"
-              value={percentageElement}
-              className="calc__input"
-              onChange={(event) => setPercentageElement(event.target.value)}/>
-          </div>
-          <div>
-            <Button>계산하기</Button>
-          </div>
+        <form id="percentage" className={cx('calc__forms')} onSubmit={submit}>
+          <Input
+            type="number"
+            id="percentage-standard"
+            value={percentage.standard}
+            className="calc__input"
+            placeholder="기준값 e.g, 1440, 750px..."
+            onChange={(event) => setPercentage({...percentage, standard: event.target.value})}/>
+          <Input
+            type="number"
+            id="percentage-standard"
+            value={percentage.element}
+            className="calc__input"
+            placeholder="요소값"
+            onChange={(event) => setPercentage({...percentage, element: event.target.value})}/>
+          <Button>Enter</Button>
         </form>
-        <div className={cx('calc__result')}>
+        <strong className={cx('calc__result')}>
           > "{percentageResult}" %
-        </div>
+        </strong>
       </CalcBox>
-    </div>
+      <CalcBox>
+        <h2>2. Root element</h2>
+        <p>e.g., @media ( min || max standard ) { `{ value }` }</p>
+        <form id="rem" className={cx('calc__forms')} onSubmit={submit}>
+          <Input
+            type="number"
+            value={rem.standard}
+            className="calc__input"
+            placeholder="기준값 e.g, 1440, 750px..."
+            onChange={(event) => setRem({...rem, standard: event.target.value })}/>
+          <Input
+            type="number"
+            value={rem.default}
+            className="calc__input"
+            placeholder="기본값 e.g, 10px"
+            onChange={(event) => setRem({...rem, default: event.target.value })}/>
+          <Input
+            type="number"
+            value={rem.relative}
+            className="calc__input"
+            placeholder="상대값 e.g, 375, 320px..."
+            onChange={(event) => setRem({...rem, relative: event.target.value })}/>
+          <Button>Enter</Button>
+        </form>
+        <strong className={cx('calc__result')}>
+          > "{remResult}" %
+        </strong>
+      </CalcBox>
+      <CalcBox>
+        <h2>3. proportional expression</h2>
+        <p>e.g., A : B = C : 𝓍</p>
+        <form id="proportion" className={cx('calc__forms')} onSubmit={submit}>
+          <Input
+            type="number"
+            value={proportion.A}
+            className="calc__input"
+            placeholder="A"
+            onChange={(event) => setProportion({...proportion, A: event.target.value})}/> :
+          <Input
+            type="number"
+            value={proportion.B}
+            className="calc__input"
+            placeholder="B"
+            onChange={(event) => setProportion({...proportion, B: event.target.value})}/> =
+          <Input
+            type="number"
+            value={proportion.C}
+            className="calc__input"
+            placeholder="C"
+            onChange={(event) => setProportion({...proportion, C: event.target.value})}/> :
+          <Input
+            type="text"
+            value={proportion.X}
+            className="calc__input"
+            placeholder="X"
+            readOnly/>
+          <Button>Enter</Button>
+        </form>
+        <strong className={cx('calc__result')}>
+          > "{proportionResult}" %
+        </strong>
+      </CalcBox>
+    </CalcWrap>
   )
 }
 
